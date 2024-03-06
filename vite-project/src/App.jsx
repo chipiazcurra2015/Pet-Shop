986ed7bb3c10f80815ea4abf7abd7bbd3a164fe5
chipiazcurra2015 <chipiazcurra2015@gmail.com>
2024-03-06 16:26:16 -0300

    first commit

---
+++ vite-project/src/App.jsx
@@ -0,0 +1,24 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+        <div class="contenedor header">
+                <div class="barra">
+                    <a class="logo" href="index.html">
+                        <h1 class="logo__nombre no-margin centrar-texto">Pet-<span class="logo__bold">Shop</span></h1>
+                    </a>
+
+                    <nav class="navegacion">
+                        <a href="nosotros.html" class="navegacion__enlace">Nosotros</a> 
+                        <a href="cursos.html" class="navegacion__enlace">Alimento</a>
+                        <a href="contacto.html" class="navegacion__enlace">Contacto</a>
+                    </nav>
+                </div>
+            </div>
+              )
+}
+
+export default App
