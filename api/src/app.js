c176db04783fd205bf5f4694cee26bc7d254661b
chipiazcurra2015 <chipiazcurra2015@gmail.com>
2024-06-02 22:57:57 -0300

    actualización de main

--- api/src/app.js
+++ api/src/app.js
@@ -1,11 +1,11 @@
 const express = require("express");
 const morgan = require("morgan");
 const mainRouter = require("./routes");
+const cors = require("cors")
 
 const app = express();
 
 app.use(morgan("dev"));
-
 app.use(mainRouter);
 app.use(cors());
 
--- api/src/handlers/productsHandlers.js
+++ api/src/handlers/productsHandlers.js
@@ -1,18 +1,31 @@
 const getProductsHandler = (req, res) => {
-    res.status(200).send("Estoy en producto");
-}
+    res.status(200).send("Estoy en productos");
+};
 
 const getProductHandler = (req, res) => {
     res.send("Estoy en producto");
-}
+};
 
-const createProductHandler = (req, res)=> {
+const createProductHandler = (req, res) => {
     res.send("Voy a crear un producto");
+};
+
+const deleteProductHandler = (req, res) => {
+    res.send("Voy a eliminar un producto");
+};
+
+const putProductsHandler = (req, res) => {
+    res.send("Voy a actualizar un producto");
+};
+
+const getProductByIdHandler = (req, res) => {
+    res.send("Voy a obtener un producto por ID");
 };
 
 module.exports = {
     createProductHandler,
     getProductsHandler,
+    getProductHandler,
     deleteProductHandler,
     putProductsHandler,
     getProductByIdHandler,
